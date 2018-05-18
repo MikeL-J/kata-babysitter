beb15917955680d43f0e453a1f476c729fcb8c83
Michael J Smith <msmit22@gmail.com>
2018-05-18 00:09:46 -0400

    cleaned up time validation tests and added error notifiction

--- test/timeUtils.test.js
+++ test/timeUtils.test.js
@@ -1,14 +1,19 @@
 const expect = require('chai').expect
 const timeUtils = require('../timeUtils.js')
 
+const startError = 'Error - Start time no sooner than 5:00pm'
+const endError = 'Error - End time is only valid from 5:00pm to 4:00am'
+const bedError = 'Error - Bed time must be after start time and by midnight'
+const startSequenceError = 'Error - Start time is preceeded by end or bed time'
+
 let startTime, endTime, bedTime
 
 describe('The babysitter time inputs', () => {
- 
+
   beforeEach(() => {
-    startTime = new Date('May 11, 2018 17:00:00')
-    endTime = new Date('May 12, 2018 04:00:00')
-    bedTime = new Date('May 11, 2018 23:00:00')
+    startTime = new Date('May 11, 2018 18:00:00')
+    endTime = new Date('May 12, 2018 03:00:00')
+    bedTime = new Date('May 11, 2018 22:00:00')
   })
 
   describe('Starting time', () => {
@@ -16,28 +21,45 @@ describe('The babysitter time inputs', () => {
     it('is valid on or after 5pm', () => {
       expect(timeUtils.isStartTimeValid(startTime)).to.be.true
     })
-    
+
     it('is invalid before 5pm', () => {
       startTime.setHours(16);
       expect(timeUtils.isStartTimeValid(startTime)).to.be.false
     })
+
+    it('returns error message for invalid start time', () => {
+      startTime.setHours(4)
+      expect(timeUtils.notifyError(startTime, endTime, bedTime)).to.equal(startError)
+    })
+
   })
-    
+
   describe('Ending time', () => {
-      
-      it('is valid if on or before 4am Or on or after 5pm', () => {
-        expect(timeUtils.isEndTimeValid(endTime)).to.be.true
-      })
-      
-      it('is invalid if after 4:00am And before 5:00pm', () => {
-        endTime.setHours(5)
-        expect(timeUtils.isEndTimeValid(endTime)).to.be.false
-      })
+
+    it('is valid on or before 4am', () => {
+      expect(timeUtils.isEndTimeValid(endTime)).to.be.true
+    })
+
+    it('is valid on or after 5pm', () => {
+      endTime.setHours(17)
+      expect(timeUtils.isEndTimeValid(endTime)).to.be.true
+    })
+
+    it('is invalid if between 4:00am and 5:00pm', () => {
+      endTime.setHours(5)
+      expect(timeUtils.isEndTimeValid(endTime)).to.be.false
+    })
+
+    it('returns error message for invalid end time', () => {
+      endTime.setHours(5)
+      expect(timeUtils.notifyError(startTime, endTime, bedTime)).to.equal(endError)
+    })
+
   })
 
   describe('Bedtime', () => {
-      
-    it('is valid if after start time And on or before Midnight', () => {
+
+    it('is valid after start time', () => {
       expect(timeUtils.isBedTimeValid(startTime, bedTime)).to.be.true
     })
 
@@ -45,10 +67,47 @@ describe('The babysitter time inputs', () => {
       bedTime.setHours(0)
       expect(timeUtils.isBedTimeValid(startTime, bedTime)).to.be.true
     })
+
+    it('is invalid between midnight and 5:00pm', () => {
+      bedTime.setHours(1)
+      expect(timeUtils.isBedTimeValid(startTime, bedTime)).to.be.false
+    })
+
+    it('returns error message for invalid bed time', () => {
+      bedTime.setHours(5)
+      expect(timeUtils.notifyError(startTime, endTime, bedTime)).to.equal(bedError)
+    })
+
+  })
+
+  describe('Start time sequence check', () => {
+
+    it('returns true if valid order', () => {
+      expect(timeUtils.isStartTimeSequenceValid(startTime,endTime,bedTime)).to.be.true
+    })
+
+    it('returns false if bedTime preceeds startTime', () => {
+      endTime = new Date('May 11, 2018 17:00:00')
+      expect(timeUtils.isStartTimeSequenceValid(startTime,endTime,bedTime)).to.be.false
+    })
     
-    it('is invalid if before start time', () => {
-      bedTime.setHours(16)
-      expect(timeUtils.isBedTimeValid(startTime,bedTime)).to.be.false
+    it('returns false if endTime preceeds startTime', () => {
+      bedTime = new Date('May 11, 2018 17:00:00')
+      expect(timeUtils.isStartTimeSequenceValid(startTime,endTime,bedTime)).to.be.false
+    })
+
+    it('returns error message for invalid start time sequence', () => {
+      endTime = new Date('May 11, 2018 17:00:00')
+      expect(timeUtils.notifyError(startTime, endTime, bedTime)).to.equal(startSequenceError)
+    })
+
+  }) 
+
+  describe.skip('Bedtime sync', () => {
+
+    it('sets bedtime to endtime when endtime comes first', () => {
+
     })
   })
+  
 })
--- timeUtils.js
+++ timeUtils.js
@@ -1,18 +1,41 @@
+const startError = 'Error - Start time no sooner than 5:00pm'
+const endError = 'Error - End time is only valid from 5:00pm to 4:00am'
+const bedError = 'Error - Bed time must be after start time and by midnight'
+const startSequenceError = 'Error - Start time is preceeded by end or bed time'
+
 let isStartTimeValid = (startTime) => {
   return startTime.getHours() >= 17
 }
 
 let isEndTimeValid = (endTime) => {
-  return endTime.getHours() <= 4 || endTime.getHours() >= 17 
+  return endTime.getHours() <= 4 || endTime.getHours() >= 17
 }
 
 let isBedTimeValid = (startTime, bedTime) => {
-  return bedTime.getHours() > startTime.getHours() || 
-         bedTime.getHours() == 0
+  return bedTime.getHours() > startTime.getHours() ||
+    bedTime.getHours() == 0
+}
+
+let isStartTimeSequenceValid = (startTime, endTime, bedTime) => {
+  return startTime <= endTime && startTime <= bedTime
+}
+
+let notifyError = (startTime, endTime, bedTime) => {
+  if (!isStartTimeValid(startTime)) {
+    return startError
+  } else if (!isEndTimeValid(endTime)) {
+    return endError
+  } else if (!isBedTimeValid(startTime, bedTime)) {
+    return bedError
+  } else if (!isStartTimeSequenceValid(startTime, endTime, bedTime)) {
+    return startSequenceError
+  }
 }
 
 module.exports = {
   isStartTimeValid: isStartTimeValid,
   isEndTimeValid: isEndTimeValid,
-  isBedTimeValid: isBedTimeValid
+  isBedTimeValid: isBedTimeValid,
+  isStartTimeSequenceValid: isStartTimeSequenceValid,
+  notifyError: notifyError
 }
